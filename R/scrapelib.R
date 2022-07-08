38f06591aa8e22453b0c1007994c74152e97175e
fawda123 <mbafs2012@gmail.com>
2022-07-08 09:22:29 -0400

    library scrape done, updated docker file

--- R/scrapelib.R
+++ R/scrapelib.R
@@ -1,9 +1,41 @@
-library(box)
-scrptlib <- function(fl){
-  
-  box::use(
-    dplyr[...]
-  )
+library(dplyr)
+library(gh)
+
+# identify repos in tbep-tech
+repos <- gh::gh(
+  "/orgs/{org}/repos",
+  org = "tbep-tech",
+  type = "all",
+  per_page = 100,
+  .limit = Inf
+  ) 
+
+# get repo topics
+repostopics <- repos %>% 
+  purrr::map(function(x){
+    name <- x$name
+    topics <- unlist(x$topics)
+    out <- list(topics)
+    names(out) <- name
+    return(out)
+  }) 
+
+# get repos with dashboard topic
+dashrepos <- repostopics %>% 
+  purrr::map_lgl(function(x) 'dashboard' %in% x[[1]]) %>% 
+  repostopics[.] %>% 
+  purrr::map(names) %>% 
+  unlist() %>% 
+  sort()
+
+'seagrasstransect/master/otbseagrass.Rmd'
+'sso-dash/master/future-risk.Rmd'
+
+# these are repos where the index is not the rmd file
+excrepos <- c('tidalcreek-dash', 'wq-dash', 'seagrasstransect', 'nekton-dash', 'tbepRSparrow-control')
+dashrepos <- dashrepos[!dashrepos %in% excrepos]
+
+scrplib <- function(fl){
   
   tmp <- readLines(url(fl))
   tmp <- grep('^library\\(', tmp, value = T)
@@ -11,14 +43,34 @@ scrptlib <- function(fl){
   out <- sapply(tmp, function(x) x %>% gsub('library\\(|\\)', '', .)) %>% 
     unlist %>% 
     unique %>% 
-    sort %>% 
-    paste(collapse = ', ')
+    sort
   
-  if(nchar(out) == 0)
-    out <- 'none'
+  if(length(out) == 0)
+    out <- NULL
   
   return(out)
   
 }
 
-fl <- 'https://raw.githubusercontent.com/tbep-tech/seagrasstransect-training-dash/master/index.Rmd'
+fls <- paste0('https://raw.githubusercontent.com/tbep-tech/', dashrepos, '/master/index.Rmd')
+
+pkgs <- purrr::map(fls, scrplib) 
+
+flsexc <- c(
+  'https://raw.githubusercontent.com/tbep-tech/tidalcreek-dash/master/tidalcreek-dash.Rmd',
+  'https://raw.githubusercontent.com/tbep-tech/wq-dash/master/wq-dash.Rmd',
+  'https://raw.githubusercontent.com/tbep-tech/nekton-dash/master/nekton-dash.Rmd',
+  'https://raw.githubusercontent.com/tbep-tech/tbepRSparrow-control/master/app.R',
+  'https://raw.githubusercontent.com/tbep-tech/sso-dash/master/future-risk.Rmd'
+  )
+excpkgs <- purrr::map(flsexc, scrplib)
+  
+pkgs <- c(pkgs, excpkgs) %>% 
+  unlist %>% 
+  unique %>% 
+  sort
+
+pkgsdkr <- paste0("install.packages('", pkgs, "', repos='http://cran.rstudio.com/')")
+pkgsdkr <- paste0('RUN R -e "', pkgsdkr, '"')
+
+# writeLines(pkgsdkr, 'tmp.txt')
